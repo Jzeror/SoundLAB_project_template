3daa2aed566070c6642f7090671c87667e1da827
brilliantnr <brilliantnr@gmail.com>
2018-10-24 16:41:43 +0900

    10/24 16:41

--- src/main/java/com/soundlab/web/admin/AdminMapper.java
+++ src/main/java/com/soundlab/web/admin/AdminMapper.java
@@ -1,6 +1,15 @@
-/*package com.soundlab.web.admin;
+package com.soundlab.web.admin;
 
-public class AdminMapper {
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminMapper {
+	public List<?> artiGS(String s);
+	public List<?> ageGenre();
+	public List<?> ageArtistK();
+	public List<?> ageArtistPivot();
 
 }
-*/
